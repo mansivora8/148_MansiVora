c92e5562b93e4b4ac68d46b6f5a073d7e51a4166
Mansi <63548294+mansivora8@users.noreply.github.com>
2021-10-08 13:07:34 +0530

    Add files via upload

---
+++ Lab2/lab2_1.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+datasets = pd.read_csv('C:/Users/HP/Documents/Sem7/ML/Lab2/L2/Datasets/Data_for_Transformation.csv')
+print("\nData : \n", datasets)
